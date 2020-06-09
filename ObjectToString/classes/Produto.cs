b9a174923e68a496bf438dbe626a42f4a30af3da
Claudio Felix <claudio.felixdossantos@yahoo.com.br>
2020-06-09 17:35:37 -0300

    Convertendo um Objeto em String utilizando ToString

---
+++ ObjectToString/classes/Produto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ObjectToString
+{
+    class Produto
+    {
+        public String nome;
+        public double preco;
+        public int qtdEstoque;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return preco * qtdEstoque;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            qtdEstoque += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            if(quantidade <= qtdEstoque)
+            {
+                qtdEstoque -= quantidade;
+            }
+            else
+            {
+                Console.WriteLine(" Quantidade no estoque menor que a solicitada ! \n");
+            }
+
+          
+        }
+
+        //Sobreposição 
+
+        // A palavra  override indica que esta operação vem de outra classe 
+
+        public override string ToString() //Retorna uma cadeira de caracteres que representa o objeto atual 
+        {//Aqui definimos como o objeto será retornado na forma de String
+            return nome +
+                ", $ " +
+                preco.ToString("F2 ", CultureInfo.InvariantCulture) +
+                qtdEstoque +
+                " unidade, total : $ " +
+                ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+        /* * ToString - converte um objeto para String  
+        * Utilizando o método ToString podemos escolher como o objeto será representado na forma de String*/
+
+    }
+}
---
+++ ObjectToString/classes/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine(" Entre com os dados do Produto \n");
+
+            Console.WriteLine(" Entre com o nome do produto :");
+            p.nome = Console.ReadLine();
+
+            Console.WriteLine(" Entre com o preço do produto :");
+            p.preco = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(" Entre com a quantidade em estoque do produto :");
+            p.qtdEstoque = int.Parse(Console.ReadLine());
+
+            mostrarProduto(p);
+
+            Console.WriteLine(" Entre com a quantidade que será adicionada ao estoque : \n");
+            p.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            mostrarProduto(p);
+
+            Console.WriteLine(" Entre com a quantidade que será removida do estoque : \n");
+            p.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            mostrarProduto(p);
+
+            Console.Read();           
+
+        }
+
+        public static void mostrarProduto(Produto p)
+        {
+            Console.WriteLine(" Dados do produto :" + p);
+        }
+    }   
+}
+
+
+
+/* Classe CultureInfo : Fornece informações sobre uma cultura especifica. As informações incluem 
+ * os nomes da cultura, o sistema de escrita, o calendario usado, a ordem de classificaçação da cadeia de 
+ * caracteres e a formatação de datas e numeros. 
+ * 
+ * O método InvariantCulture : Obtem o objeto CultureInfo que é independente de cultura(invariavel) * 
+   */
+
+/* Toda classe em c# é uma subclasse da classe Object 
+ * Object possui os seguintes métodos :
+ * GetType - retorna o tipo do objeto 
+ * Equals - compara se um objeto é igual ao outro
+ * GetHashCode - retorna um codigo hash do objeto 
+
+
+ */
